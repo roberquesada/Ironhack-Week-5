f382894e031e41656a39fccf4df8ba604219723d
Roberto Quesada <robertoquesadalizana@gmail.com>
2015-11-23 16:19:23 +0100

    Add iteration 2 to jquery-exercise

--- Day1/jquery-exercise/js/script.js
+++ Day1/jquery-exercise/js/script.js
@@ -4,10 +4,19 @@ var pharses = [
   'Make sure to folow all about the last pharse with a comma.'
 ];
 
-var randomNumber = getRandomInt(0,2);
-
 function getRandomInt(min, max) {
-  return Math.floor(Math.random() * (max - min)) + min;
+  return Math.round(Math.random() * (max - min)) + min;
 }
 
-$('#pharse').text(pharses[randomNumber]);
+function changePharse() {
+  var randomNumber = getRandomInt(0,2);
+  $('#pharse').text(pharses[randomNumber]);
+}
+
+function init(){
+  changePharse();
+}
+
+init();
+
+$(document).on('click', '#button-pharse', changePharse);
